7bfa54c30c24ae5a795e0bef7df99fab89b93e19
BuschMath <busch@iowacentral.edu>
2024-10-10 10:06:33 -0500

    Checking for vaild input stream.

--- Ch5_Example_FA24/Main.cpp
+++ Ch5_Example_FA24/Main.cpp
@@ -1,61 +1,21 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
 int main()
 {
-	cout << "Do you want to determine your outcome by letter or number?"; 
-	cout << "Enter l for letter or n for number.\n";
-	char test;
-	bool letter_grade;
+	int h, w;
+	ifstream infile;
+	infile.open("input.dat");
 
-	cin >> test;
-
-	if (test == 'l')
+	if (!infile)
 	{
-		letter_grade = true;
-	}
-	else if (test == 'n')
-	{
-		letter_grade = false;
+		cout << "Cannot open file!\n";
+		return 1;	// terminate the program
 	}
 
-	if (letter_grade)
-	{
-		char Grade;
-		cout << "Enter your grade: ";
-		cin >> Grade;
-		cout << endl;
-
-		if (Grade == 'A' || Grade == 'B' || Grade == 'C' || Grade == 'a' || 
-			Grade == 'b' || Grade == 'c')
-		{
-			cout << "Congrats! You passed. \n\n";
-		}
-		else
-		{
-			cout << "That's sad. You failed. \n\n";
-		}
-	}
-	else
-	{
-		double Grade_Number;
-		cout << "Enter your grade: ";
-		cin >> Grade_Number;
-
-		if (Grade_Number >= 70)
-		{
-			cout << "Congrats! You passed. \n\n";
-			if (Grade_Number > 100)
-			{
-				cout << "Warning: Grade is greater than 100. \n\n";
-			}
-		}
-		else
-		{
-			cout << "That's sad. You failed. \n\n";
-		}
-	}
+	infile >> h >> w;
 
 	return 0;
 }
